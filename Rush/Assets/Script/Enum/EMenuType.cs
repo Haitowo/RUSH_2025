eaed88bd5b608c65ffaf39ff270f6f88eb8e0ee4
Florian MAJCHER <f.majcher@student.isartdigital.com>
2025-11-16 22:55:55 +0100

    [ADD] Menus system working : TODO - Create all menus related

---
+++ Rush/Assets/Script/Enum/EMenuType.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author : Florian MAJCHER - Isart DIGITAL
+// DATE : 00/00/0000 - Beginning of the class
+
+namespace Com.IsartDigital.Rush.UI
+{
+    
+    public enum EMenuType
+    {
+        MAIN,
+        PLAY,
+        OPTIONS,
+        CREDITS,
+        QUIT
+    }
+}
